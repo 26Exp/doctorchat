b0fe52fb46a0d97aba1a72367b9c834c2d0139cf
Novac Denis <denisnovac2017@gmail.com>
2022-11-13 13:01:37 +0200

    fixed primary menu

--- wp-content/themes/doctorchat/resources/views/sections/header.blade.php
+++ wp-content/themes/doctorchat/resources/views/sections/header.blade.php
@@ -14,12 +14,11 @@
       <a href="{{ home_url('/') }}">
         <img class="w-36 xl:w-60" src="@asset('svgs/logo.svg')" alt="Doctorchat Logo" />
       </a>
-      <nav class="hidden space-x-12 xl:block">
-        <a class="navigation-link" href="#how-it-works">Cum functioneaza</a>
-        <a class="navigation-link" href="#online-doctors">Doctori Online</a>
-        <a class="navigation-link" href="#for-doctors">Pentru Doctori</a>
-        <a class="navigation-link" href="#blog">Blog</a>
-      </nav>
+      @if (has_nav_menu('primary_navigation'))
+        <nav class="hidden space-x-12 xl:block" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
+          {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'navigation-link', 'echo' => false]) !!}
+        </nav>
+      @endif
       <div class="hidden items-center space-x-6 xl:flex">
         <a class="navigation-link" href="#ro">Ro</a>
         <a class="navigation-link" href="#ru">Ru</a>
