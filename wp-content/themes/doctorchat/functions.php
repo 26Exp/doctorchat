40ce572ff9bf3de9ed40113985d8a484a33abfa6
Anton Gadimba <antonhck@gmail.com>
2022-11-14 18:54:54 +0200

    Added Favicon

--- wp-content/themes/doctorchat/functions.php
+++ wp-content/themes/doctorchat/functions.php
@@ -114,3 +114,14 @@ function SearchFilter($query) {
 }
 add_filter('pre_get_posts','SearchFilter');
 
+function disable_real_mime_check( $data, $file, $filename, $mimes ) {
+    $wp_filetype = wp_check_filetype( $filename, $mimes );
+
+    $ext = $wp_filetype['ext'];
+    $type = $wp_filetype['type'];
+    $proper_filename = $data['proper_filename'];
+
+    return compact( 'ext', 'type', 'proper_filename' );
+}
+add_filter( 'wp_check_filetype_and_ext', 'disable_real_mime_check', 10, 4 );
+
--- wp-content/themes/doctorchat/index.php
+++ wp-content/themes/doctorchat/index.php
@@ -3,11 +3,11 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-    <link rel="manifest" href="/site.webmanifest">
-    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#79d8d5">
+    <link rel="apple-touch-icon" sizes="180x180" href="/wp-content/themes/doctorchat/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/wp-content/themes/doctorchat/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/wp-content/themes/doctorchat/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/wp-content/themes/doctorchat/favicon/site.webmanifest">
+    <link rel="mask-icon" href="/wp-content/themes/doctorchat/favicon/safari-pinned-tab.svg" color="#79d8d5">
     <meta name="msapplication-TileColor" content="#79d8d5">
     <meta name="theme-color" content="#79d8d5">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css" />
