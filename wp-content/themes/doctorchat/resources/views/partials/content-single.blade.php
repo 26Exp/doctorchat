6813136429c53453101eee09aba86db0e1cc5037
Anton Gadimba <antonhck@gmail.com>
2022-11-14 15:34:34 +0200

    Added mobile adaptation #4

--- wp-content/themes/doctorchat/resources/views/partials/content-single.blade.php
+++ wp-content/themes/doctorchat/resources/views/partials/content-single.blade.php
@@ -1,6 +1,7 @@
 <div class="max-w-3xl mx-auto my-7">
   <article @php(post_class('prose prose-doctorchat-mint py-7 mx-5 prose prose-doctorchat-mint md:prose-xl'))>
     <header>
+      <img src="{{ get_the_post_thumbnail_url(get_the_ID(), 'full') }}" alt="{{ get_the_title() }}" />
       <h1 class="entry-title">
         {!! $title !!}
       </h1>
