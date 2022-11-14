889b2a56a4cf0e3ff20139ee8794ea8d1fdee423
Anton Gadimba <antonhck@gmail.com>
2022-11-14 21:48:13 +0200

    Prod first

--- wp-content/themes/doctorchat/index.php
+++ wp-content/themes/doctorchat/index.php
@@ -27,7 +27,6 @@
   <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W5ZWSFM"
                     height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
   <!-- End Google Tag Manager (noscript) -->
-
     <?php wp_body_open(); ?>
     <?php do_action('get_header'); ?>
 
