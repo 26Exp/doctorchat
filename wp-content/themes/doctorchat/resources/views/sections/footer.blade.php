ded54587ac03eedcb414740b491d2dd277fed11f
Anton Gadimba <antonhck@gmail.com>
2023-04-26 12:33:25 +0300

    Added dynamic social links data

--- wp-content/themes/doctorchat/resources/views/sections/footer.blade.php
+++ wp-content/themes/doctorchat/resources/views/sections/footer.blade.php
@@ -11,20 +11,20 @@
         <div class="max-w-xs [&>div[class$='widget']]:space-y-4">
           @php(dynamic_sidebar('footer-one'))
           <div class="!mt-4 inline-grid grid-cols-3 gap-3 md:grid-cols-6 lg:!mt-6 ">
-            <a href="viber://chat?number=+37378272887" target="_blank">
+            <a href="{{ get_field('viber_url', 'options') }}" target="_blank">
               <div
                 class="flex h-10 w-10 items-center justify-center rounded-full bg-white p-2">
                 <img src="@asset('svgs/viber.svg')" alt="Viber"/>
               </div>
             </a>
-            <a href="https://wa.me/37378272887" target="_blank">
+            <a href="{{ get_field('whatsapp_url', 'options') }}" target="_blank">
               <div
                 class="flex h-10 w-10 items-center justify-center rounded-full bg-white p-2"
               >
                 <img src="@asset('svgs/whatsapp.svg')" alt="Whatsapp"/>
               </div>
             </a>
-            <a href="https://t.me/doctorchatmd" target="_blank">
+            <a href="{{ get_field('telegram_url', 'options') }}" target="_blank">
               <div
                 class="flex h-10 w-10 items-center justify-center rounded-full bg-white p-2"
               >
@@ -38,14 +38,14 @@
             {{--      <img src="@asset('svgs/linkedin-in.svg')" alt="Linkedin"/>--}}
             {{--    </div>--}}
             {{--  </a>--}}
-            <a href="https://www.facebook.com/doctorchat.md" target="_blank">
+            <a href="{{ get_field('facebook_url', 'options') }}" target="_blank">
               <div
                 class="flex h-10 w-10 items-center justify-center rounded-full bg-white p-3"
               >
                 <img src="@asset('svgs/facebook-f.svg')" alt="Facebook"/>
               </div>
             </a>
-            <a href="https://instagram.com/doctorchat.md" target="_blank">
+            <a href="{{ get_field('instagram_url', 'options') }}" target="_blank">
               <div
                 class="flex h-10 w-10 items-center justify-center rounded-full bg-white p-2"
               >
